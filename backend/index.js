aab188bdf2d79c3953407f7e7eabdbfa84c85d1b
CodeBrij <145279132+CodeBrij@users.noreply.github.com>
2025-01-22 20:32:42 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -162,13 +162,6 @@ app.get("/article/:id", async (req,res) => {
   res.send(result);
 })
 
-if(process.env.NODE_ENV==="production"){
-  app.use(express.static(path.join(__dirname, "../frontend/dist")))
-  app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"))
-  })
-}
-
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
